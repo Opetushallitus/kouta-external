dd72f1413dee6e4e66ecac2c991292b96bdfa02d
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2022-02-28 09:08:30 +0200

    KTO-210 & KTO-1347: Lisätty koulutustyypit amm-muu ja aikuisten perusopetus
    Otettu käyttöön ElasticDump -pohjainen testauslogiikka

--- src/test/scala/fi/oph/kouta/external/integration/KoulutusSpec.scala
+++ src/test/scala/fi/oph/kouta/external/integration/KoulutusSpec.scala
@@ -18,11 +18,11 @@ class KoulutusSpec extends KoulutusFixture with GenericGetTests[Koulutus, Koulut
 
   val nonExistingSessionId: UUID = UUID.fromString("9267884f-fba1-4b85-8bb3-3eb77440c197")
 
-  val ophKoulutusOid: KoulutusOid =           KoulutusOid("1.2.246.562.13.00000000000000000002")
-  val julkinenOid: KoulutusOid =              KoulutusOid("1.2.246.562.13.00000000000000000003")
-  val tarjoajaOid: KoulutusOid =              KoulutusOid("1.2.246.562.13.00000000000000000004")
-  val ammMuuOid: KoulutusOid =                KoulutusOid("1.2.246.562.13.00000000000000000005")
-  val aikuistenPerusopetusOid: KoulutusOid =  KoulutusOid("1.2.246.562.13.00000000000000000006")
+  val ophKoulutusOid: KoulutusOid = KoulutusOid("1.2.246.562.13.00000000000000000002")
+  val julkinenOid: KoulutusOid = KoulutusOid("1.2.246.562.13.00000000000000000003")
+  val tarjoajaOid: KoulutusOid = KoulutusOid("1.2.246.562.13.00000000000000000004")
+  val ammMuuOid: KoulutusOid = KoulutusOid("1.2.246.562.13.00000000000000000005")
+  val aikuistenPerusopetusOid: KoulutusOid = KoulutusOid("1.2.246.562.13.00000000000000000006")
 
   val sorakuvausId: UUID         = UUID.fromString("9267884f-fba1-4b85-8bb3-3eb77440c197")
 

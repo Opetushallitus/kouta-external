417202d073836c205bf16c8e34c136605495ba52
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-11-14 20:06:39 +0200

    Poistettu turhaksi jäänyt TempDb

--- src/test/scala/fi/oph/kouta/external/TempDockerDb.scala
+++ src/test/scala/fi/oph/kouta/external/TempDockerDb.scala
@@ -2,6 +2,22 @@ package fi.oph.kouta.external
 
 import fi.vm.sade.utils.slf4j.Logging
 
+object TempDbUtils {
+
+  import scala.annotation.tailrec
+
+  @tailrec
+  def tryTimes(times: Int, sleep: Int)(thunk: () => Boolean): Boolean = times match {
+    case n if n < 1 => false
+    case 1          => thunk()
+    case n =>
+      thunk() || {
+        Thread.sleep(sleep);
+        tryTimes(n - 1, sleep)(thunk)
+      }
+  }
+}
+
 object TempDockerDb extends Logging {
 
   import TempDbUtils.tryTimes

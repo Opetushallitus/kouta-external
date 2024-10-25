75080a199959f26c5957b9a731e0bd41a926c19f
August Kilponen <august.kilponen@gmail.com>
2024-10-25 19:34:49 +0300

    Muutettu käyttämään suoraan AWS SDK:ta

--- europass-publisher/src/main/scala/fi/oph/kouta/europass/EuropassPublisherApp.scala
+++ europass-publisher/src/main/scala/fi/oph/kouta/europass/EuropassPublisherApp.scala
@@ -2,15 +2,18 @@ package fi.oph.kouta.europass
 
 import fi.vm.sade.valinta.dokumenttipalvelu.Dokumenttipalvelu
 import fi.vm.sade.utils.slf4j.Logging
+
 import scala.compat.java8.FutureConverters._
 import scala.concurrent.Future
-import java.io.{BufferedInputStream, FileInputStream}
+import java.io.{BufferedInputStream, File, FileInputStream}
 import java.net.URI
-import software.amazon.awssdk.auth.credentials.{AwsBasicCredentials, StaticCredentialsProvider, ProfileCredentialsProvider}
+import software.amazon.awssdk.auth.credentials.{AwsBasicCredentials, ProfileCredentialsProvider, StaticCredentialsProvider, SystemPropertyCredentialsProvider}
+import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.regions.Region
-import software.amazon.awssdk.services.s3.model.PutObjectResponse
-import software.amazon.awssdk.services.s3.S3AsyncClient
+import software.amazon.awssdk.services.s3.model.{PutObjectRequest, PutObjectResponse}
+import software.amazon.awssdk.services.s3.{S3AsyncClient, S3Client}
 import software.amazon.awssdk.services.s3.presigner.S3Presigner
+
 import scala.concurrent.duration._
 import scala.concurrent.Await
 
@@ -21,40 +24,37 @@ object EuropassPublisherApp extends Logging {
   val awsBucket = EuropassConfiguration.config.getString("europass-publisher.s3.bucketname")
   val s3Key = EuropassConfiguration.config.getString("europass-publisher.s3.keyname")
   val awsEndpointOverride = EuropassConfiguration.config.getString("europass-publisher.s3.endpoint")
-  lazy val dokumenttipalvelu = getDokumenttipalvelu()
+  lazy val s3Client = getS3Client()
 
-  def getDokumenttipalvelu() = {
-    awsEndpointOverride match {
-      case url if url.startsWith("http") =>
-        new Dokumenttipalvelu(awsRegion, awsBucket) {
-          override def getClient() =
-            S3AsyncClient.builder()
-              .credentialsProvider(StaticCredentialsProvider.create(
-                AwsBasicCredentials.create("none", "none")
-              ))
-              .region(Region.of(awsRegion))
-              .endpointOverride(new URI(url))
-              .build()
-          override def getPresigner() =
-            S3Presigner.builder()
-              .credentialsProvider(StaticCredentialsProvider.create(
-                AwsBasicCredentials.create("none", "none")
-              ))
-              .region(Region.of(awsRegion))
-              .endpointOverride(new URI(url))
-              .build()
-        }
-      case _ => new Dokumenttipalvelu(awsRegion, awsBucket)
-    }
+  def getS3Client() = {
+    S3Client.builder()
+      .endpointOverride(new URI(awsEndpointOverride))
+      .region(Region.of(awsRegion))
+      .credentialsProvider(StaticCredentialsProvider.create(
+        AwsBasicCredentials.create("none", "none")
+      ))
+      .forcePathStyle(true)
+      .build()
   }
-  
+
   def main(args : Array[String]) {
     val future: Future[PutObjectResponse] = Publisher.publishedToteutuksetAsFile()
-      .flatMap{fileName: String => {
-        logger.info(s"Toteutukset dumped in $fileName")
-        toScala(dokumenttipalvelu.putObject(
-          s3Key, fileName, "application/xml", new BufferedInputStream(new FileInputStream(fileName))
-        ))
+      .flatMap{
+        fileName: String => {
+          logger.info(s"Toteutukset dumped in $fileName")
+          //toScala(dokumenttipalvelu.putObject(
+          //  s3Key, fileName, "application/xml", new BufferedInputStream(new FileInputStream(fileName))
+          //))
+          val putObjectResponse = s3Client.putObject(
+            PutObjectRequest
+              .builder()
+              .bucket(awsBucket)
+              .key(s3Key)
+              .contentType("application/xml")
+              .build(),
+              RequestBody.fromFile(new File(fileName))
+          )
+          Future.successful(putObjectResponse)
       }}
     val result = Await.result(future, 60.minute)
     logger.info(s"Uploaded to S3 with result $result")

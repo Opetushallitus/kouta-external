52012858396ce14b585f1d1a9387763fa77f9404
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2024-11-22 10:01:51 +0200

    OK-636: move europass-publisher.properties.template to correct name

--- europass-publisher/src/main/scala/fi/oph/kouta/europass/EuropassConfiguration.scala
+++ europass-publisher/src/main/scala/fi/oph/kouta/europass/EuropassConfiguration.scala
@@ -1,8 +1,17 @@
 package fi.oph.kouta.europass
 
+import fi.vm.sade.utils.slf4j.Logging
 import com.typesafe.config.ConfigFactory
 
-object EuropassConfiguration {
-  lazy val config = ConfigFactory.load("europass-publisher")
-    .withFallback(ConfigFactory.load("default"))
+object EuropassConfiguration extends Logging {
+  val templatedConfig = ConfigFactory.load("kouta-external-europass")
+  //val templatedConfigEP = templatedConfig.getConfig("europass-publisher")
+  logger.info(s"Templated config is $templatedConfig")
+  val localConfig = ConfigFactory.load("europass-publisher")
+  //val localConfigEP = localConfig.getConfig("europass-publisher")
+  logger.info(s"Local config is $localConfig")
+  val fallbackConfig = ConfigFactory.load("default")
+  //val fallbackConfigEP = fallbackConfig.getConfig("europass-publisher")
+  logger.info(s"Fallback config is $fallbackConfig")
+  lazy val config = templatedConfig.withFallback(localConfig).withFallback(fallbackConfig)
 }

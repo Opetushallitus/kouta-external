49d5a853c80bd70cecc6d94396578de5e2d8beaa
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2024-11-25 09:56:56 +0200

    OK-636: print classpath on startup for debugging

--- europass-publisher/src/main/scala/fi/oph/kouta/europass/EuropassConfiguration.scala
+++ europass-publisher/src/main/scala/fi/oph/kouta/europass/EuropassConfiguration.scala
@@ -4,6 +4,15 @@ import fi.vm.sade.utils.slf4j.Logging
 import com.typesafe.config.ConfigFactory
 
 object EuropassConfiguration extends Logging {
+  def classPathUrls(cl: ClassLoader): Array[java.net.URL] = cl match {
+    case null => Array()
+    case u: java.net.URLClassLoader => u.getURLs() ++ classPathUrls(cl.getParent)
+    case _ => classPathUrls(cl.getParent)
+  }
+
+  logger.info("Reading configuration from classpath:")
+  logger.info(classPathUrls(getClass.getClassLoader).mkString(", "))
+
   val templatedConfig = ConfigFactory.load("kouta-external-europass")
   //val templatedConfigEP = templatedConfig.getConfig("europass-publisher")
   logger.info(s"Templated config is $templatedConfig")

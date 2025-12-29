adbf2986de25212b3979e727c5ee0044d7a2cdcc
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2025-12-29 15:40:17 +0200

    OK-1091: Parannellaan käyttöoikeustarkastuksia

--- kouta-external/src/main/scala/fi/oph/kouta/external/servlet/KoutaLightServlet.scala
+++ kouta-external/src/main/scala/fi/oph/kouta/external/servlet/KoutaLightServlet.scala
@@ -3,13 +3,17 @@ package fi.oph.kouta.external.servlet
 import fi.oph.kouta.external.domain.koutalight.KoutaLightKoulutus
 import fi.oph.kouta.external.service.KoutaLightService
 import fi.oph.kouta.external.swagger.SwaggerPaths.registerPath
+import fi.oph.kouta.security.Role
 import fi.oph.kouta.security.Role.Paakayttaja
 import fi.oph.kouta.servlet.Authenticated
-import org.scalatra.ActionResult
+import org.scalatra.{Forbidden, Ok}
+
+import scala.util.{Failure, Success, Try}
 
 object KoutaLightServlet extends KoutaLightServlet(KoutaLightService)
 
 class KoutaLightServlet(koutaLightService: KoutaLightService) extends KoutaServlet with CasAuthenticatedServlet {
+  private def errorMessage(message: String) = Map("errorMessage" -> message)
 
   registerPath(
     "/koutan-tietomallista-poikkeavat-koulutukset",
@@ -39,17 +43,37 @@ class KoutaLightServlet(koutaLightService: KoutaLightService) extends KoutaServl
       |                $ref: '#/components/schemas/KoutaLightMassResult'
       |        '401':
       |          description: Unauthorized
-      |
+      |        '403':
+      |          description: Forbidden
       |""".stripMargin
   )
   put("/") {
     if (externalModifyEnabled) {
       implicit val authenticated: Authenticated = authenticate
-      // Käytetään pääkäyttäjän organisaatiota kunnes saadaan oma käyttöoikeus
-      val authorizedOrg = authenticated.session.getOrganizationsForRoles(Seq(Paakayttaja)).head
-      koutaLightService.put(parsedBody.extract[List[KoutaLightKoulutus]], authorizedOrg)
+
+      // Käytetään koulutuksen omistavana organisaationa pääkäyttäjän organisaatiota kunnes saadaan oma käyttöoikeus
+      Try(authenticated.session.getOrganizationsForRoles(Seq(Paakayttaja))) match {
+        // Käyttäjällä saa olla vain yksi organisaatio liitettynä käyttöoikeuteen
+        case Success(orgs) if orgs.size == 1 =>
+          val ownerOrg         = orgs.head
+          // Vain OphPääkäyttäjä voi tallentaa koulutuksia rajapinnan kautta kunnes saadaan oma käyttöoikeus
+          val isOphPaakayttaja = authenticated.session.roles.contains(Role.Paakayttaja)
+
+          if (isOphPaakayttaja) {
+            Ok(koutaLightService.put(parsedBody.extract[List[KoutaLightKoulutus]], ownerOrg))
+          } else {
+            Forbidden(errorMessage("Käyttäjällä ei ole oikeutta koulutusten tallentamiseen rajapinnan kautta"))
+          }
+        case Success(orgs) if orgs.size > 1 =>
+          Forbidden(errorMessage("Käyttäjän oikeuksissa määritelty liian monta organisaatiota"))
+        case Success(orgs) if orgs.isEmpty =>
+          Forbidden(errorMessage("Käyttäjän oikeuksissa ei ole määritelty organisaatiota"))
+        case Failure(e) =>
+          logger.warn(e.toString)
+          Forbidden(errorMessage("Käyttäjän oikeuksissa puutteita"))
+      }
     } else {
-      ActionResult(403, "Rajapinnan käyttö estetty tässä ympäristössä", Map.empty)
+      Forbidden(errorMessage("Rajapinnan käyttö estetty tässä ympäristössä"))
     }
   }
 }

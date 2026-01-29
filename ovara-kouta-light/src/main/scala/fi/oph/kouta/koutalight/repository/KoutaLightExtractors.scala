fb2b978264a4d6852d5df31f2cb7e45cc97646b8
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2026-01-29 13:43:55 +0200

    OK-1091: Yhdistetään database- ja repository-koodit

--- ovara-kouta-light/src/main/scala/fi/oph/kouta/koutalight/repository/KoutaLightExtractors.scala
+++ ovara-kouta-light/src/main/scala/fi/oph/kouta/koutalight/repository/KoutaLightExtractors.scala
@@ -2,14 +2,14 @@ package fi.oph.kouta.koutalight.repository
 
 import fi.oph.kouta.domain.Kieli
 import fi.oph.kouta.domain.oid.OrganisaatioOid
-import fi.oph.kouta.koutalight.domain.{Kielistetty, KoutaLightKoulutusMetadata, KoutaLightKoulutusWithMetadata}
+import fi.oph.kouta.koutalight.domain.{Kielistetty, KoutaLightKoulutusMetadata, KoutaLightKoulutusWithMetadata, SiirtotiedostoOperation}
 import fi.oph.kouta.koutalight.util.KoutaLightJsonFormats
 import org.json4s.jackson.Serialization.read
 import slick.jdbc.GetResult
 
 import java.util.UUID
 
-trait Extractors extends KoutaLightJsonFormats {
+trait KoutaLightExtractors extends KoutaLightJsonFormats {
   private def extractKielivalinta(json: Option[String]): Seq[Kieli] = json.map(read[Seq[Kieli]]).getOrElse(Seq())
   private def extractKielistetty(json: Option[String]): Kielistetty =
     json.map(read[Map[Kieli, String]]).getOrElse(Map())
@@ -41,6 +41,18 @@ trait Extractors extends KoutaLightJsonFormats {
         ownerOrg = ownerOrg,
         createdAt = createdAt,
         updatedAt = updatedAt
+      )
+    })
+
+  implicit val getLatestSiirtotiedostoOperationResult: GetResult[SiirtotiedostoOperation] =
+    GetResult(r => {
+      SiirtotiedostoOperation(
+        id = UUID.fromString(r.nextString()),
+        windowStart = r.nextTimestampOption().map(_.toInstant),
+        windowEnd = r.nextTimestamp().toInstant,
+        runStart = r.nextTimestamp().toInstant,
+        runEnd = r.nextTimestamp().toInstant,
+        storedEntitiesCount = r.nextInt()
       )
     })
 }

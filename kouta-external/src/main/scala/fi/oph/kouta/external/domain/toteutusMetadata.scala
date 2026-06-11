209ba7c1c1896b8f28e5ce22e590790a390d866a
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2026-06-11 15:06:10 +0300

    OY-5674: Lisää korjauksia swagger-dokumentaatioon

--- kouta-external/src/main/scala/fi/oph/kouta/external/domain/toteutusMetadata.scala
+++ kouta-external/src/main/scala/fi/oph/kouta/external/domain/toteutusMetadata.scala
@@ -431,7 +431,7 @@ case class LukiodiplomiTieto(koodiUri: String, linkki: Kielistetty, linkinAltTek
     |              description: Koulutuksen kielivalikoima
     |              $ref: '#/components/schemas/Kielivalikoima'
     |            yleislinja:
-    |              type: boolean,
+    |              type: boolean
     |              description: Onko lukio-toteutuksella yleislinja?
     |            painotukset:
     |              type: array
